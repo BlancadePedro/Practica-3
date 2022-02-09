854349e2e833004ae9ee7e621bc4bcdeca0b89bb
BlancadePedro <blancadepedr@gmail.com>
2022-02-09 19:43:32 +0100

    Practica 3 css

---
+++ js/demo/chart-pie-demo.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Europa", "América", "Asia"],
+    datasets: [{
+      data: [4554, 790, 1323],
+      backgroundColor: ['#0d6efd', '#0dcaf0', '#20c997'],
+      hoverBackgroundColor: ['#6610f2', '#adb5db', '#198754'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 20,
+      yPadding: 20,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
